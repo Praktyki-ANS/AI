f97e9b4b9c2adfcb1942ac0a2323a625c41d4434
Bajej2143 <blazej.d.2001@gmail.com>
2025-01-07 17:55:27 +0100

    poprawka bo git się nie zgitował

--- routes/translate.py
+++ routes/translate.py
@@ -26,3 +26,23 @@ def translate_text_en_pl(request: RequestModel):
         return {"input": input_text, "translation": translated_text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Translation failed: {str(e)}")
+
+@router.post("/translate/en-esp")
+def translate_text_en_esp(request: RequestModel):
+    input_text = request.input
+    try:
+        translation = models["en-esp"](input_text, max_length=40, return_tensors=False)
+        translated_text = translation[0]["translation_text"]
+        return {"input": input_text, "translation": translated_text}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Translation failed: {str(e)}")
+
+@router.post("/translate/en-ger")
+def translate_text_en_ger(request: RequestModel):
+    input_text = request.input
+    try:
+        translation = models["en-ger"](input_text, max_length=40, return_tensors=False)
+        translated_text = translation[0]["translation_text"]
+        return {"input": input_text, "translation": translated_text}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Translation failed: {str(e)}")
