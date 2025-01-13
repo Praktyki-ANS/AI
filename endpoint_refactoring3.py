4ba198516f926fb2a5deaff21d361e3d156a6f3f
SixPix <20199@student.ans-elblag.pl>
2025-01-13 09:36:16 +0100

    endpoint_refactoring3.py

--- endpoint_refactoring3.py
+++ endpoint_refactoring3.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI, HTTPException, Depends
+from refactoring_DialoGPT import generate_text3 
 
+
+app = FastAPI()
+
+
+@app.post("/generate-text3")
+def generate_text_endpoint(request: RequestModel):
+    try:
+        result = generate_text(request.input, models)
+        return result
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        return {"error": str(e)}
