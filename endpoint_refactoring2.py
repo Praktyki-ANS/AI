9666983177084324a04db114284e31e744d2e665
SixPix <20199@student.ans-elblag.pl>
2025-01-13 09:31:38 +0100

    endpoint_refactoring2.py

--- endpoint_refactoring2.py
+++ endpoint_refactoring2.py
@@ -4,12 +4,12 @@ from refactoring_gpt_neo import generate_text2
 
 app = FastAPI()
 
-@app.post("/generate-text2")
-def generate_text_endpoint(request: RequestModel, api_key: str = Depends(get_api_key)):
+
+@app.post("/generate-text")
+def generate_text_endpoint(request: RequestModel):
     try:
-        # Wywołanie funkcji generującej tekst z wykorzystaniem klucza API
-        result = generate_text2(request.input, api_key)
-        return {"result": result}
+        result = generate_text(request.input, models)
+        return result
     except HTTPException as http_exc:
         raise http_exc
     except Exception as e:
