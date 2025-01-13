2ddeba85121577a58ead35e7636b99fbdc2b2dde
wynalazca382 <20179@student.ans-elblag.pl>
2025-01-13 08:29:41 +0100

    Czysty front

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from backend import app
-
-client = TestClient(app)
-
-def test_healthcheck():
-    response = client.get("/healthcheck")
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
