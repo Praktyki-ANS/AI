a114c570403d76f9ff95ad80cc6bbd90149097a7
wynalazca382 <20179@student.ans-elblag.pl>
2025-01-15 08:13:02 +0100

    Zmieniono modele

--- models_registry.py
+++ models_registry.py
@@ -17,16 +17,15 @@ def load_models():
     models["emotion_classifier"] = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
     models["question_answering"] = pipeline("question-answering", model="deepset/roberta-base-squad2")
     models["text_generation"] = pipeline("text-generation", model="gpt2")
-    models["text_generation2"] = pipeline("text-generation", model="gpt_neo")
-    models["text_generation3"] = pipeline("text-generation", model="DialoGPT")
+    models["text_generation2"] = pipeline("text-generation", model="EleutherAI/gpt-neo-125M")
+    models["text_generation3"] = pipeline("text-generation", model="microsoft/DialoGPT-medium")
     models["toxicity_classifier"] = pipeline("text-classification", model="unitary/toxic-bert")
     models["paraphrasing_model"] = pipeline("text2text-generation", model="Vamsi/T5_Paraphrase_Paws")
     models["sentiment_analysis"] = pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
     models["topic_classifier"] = pipeline("text-classification", model="facebook/bart-large-mnli")
     models["zero_shot_classifier"] = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
     models["ner_model"] = pipeline("ner", model="dbmdz/bert-large-cased-finetuned-conll03-english")
-    models["en-ger"] = pipeline("translation", model="Helsinki-NLP/opus-mt-en-de")
-    models["en-esp"] = pipeline("translation", model="Helsinki-NLP/opus-mt-en-es")
+
 
     # Load Stable Diffusion model
     models["stable_diffusion"] = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
