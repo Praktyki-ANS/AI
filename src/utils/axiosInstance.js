416b95e8c2b6aebbeb02d23803887e0049ffdd86
Shad0w <kejkej120@gmail.com>
2025-01-23 12:33:01 +0100

    new route for backend

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://10.100.24.254:3000', // Backend URL
+    baseURL: 'http://ai-o0vr.onrender.com/', // Backend URL
     headers: {
         'Content-Type': 'application/json',
     },
